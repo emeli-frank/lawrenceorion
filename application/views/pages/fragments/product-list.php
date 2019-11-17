82334a0d00d1abe1eacab575eb02c397cca5c83a
Frank Emeli <dev.carlfrank@gmail.com>
2019-11-17 17:31:43 +0100

    Removed tab in the product list section

--- application/views/pages/fragments/product-list.php
+++ application/views/pages/fragments/product-list.php
@@ -10,59 +10,32 @@
 
 <div class=""><?=$pagination_view?></div>
 
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-    <li class="nav-item">
-        <a class="nav-link active" id="customer-view-tab" data-toggle="tab" href="#customer-view" role="tab" aria-controls="customer-view" aria-selected="true">Grid View</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" id="manage-view-tab" data-toggle="tab" href="#manage-view" role="tab" aria-controls="manage-view" aria-selected="false">List View</a>
-    </li>
-</ul>
-<div class="tab-content my-3" id="myTabContent">
-    <div class="tab-pane fade show active" id="customer-view" role="tabpanel" aria-labelledby="customer-view-tab">
-        <div class="product-list-containter">
-            <?php foreach ($products as $product): ?>
-                <a href="/categories/<?=$product->category_id?>/products/<?=$product->id?>" class="product-tile">
-                    <div class="card" _style="width: 12rem;">
-                        <img src="/assets/images/products/product-placeholder-image.jpg" class="card-img-top">
-                        <div class="card-body">
-                            <!-- <h5 class="card-title"><?=$product->name?></h5> -->
-                            <div class="product-name"><?=$product->name?></div>
-                            <div class="price">
-                                <span class="price-current">
-                                    <span data-currency-iso="NGN">₦</span>
-                                    <span><?=$product->price?></span>
-                                </span>
-                                <span class="price-old ">
-                                    <span data-currency-iso="NGN">₦</span>
-                                    <span><?=$product->old_price?></span>
-                                </span>
-                            </div>
-                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                        </div>
+<div class="product-list-containter">
+    <?php foreach ($products as $product): ?>
+        <a href="/categories/<?=$product->category_id?>/products/<?=$product->id?>" class="product-tile">
+            <div class="card" _style="width: 12rem;">
+                <img src="/assets/images/products/product-placeholder-image.jpg" class="card-img-top">
+                <div class="card-body">
+                    <!-- <h5 class="card-title"><?=$product->name?></h5> -->
+                    <div class="product-name"><?=$product->name?></div>
+                    <div class="price">
+                        <span class="price-current">
+                            <span data-currency-iso="NGN">₦</span>
+                            <span><?=$product->price?></span>
+                        </span>
+                        <span class="price-old ">
+                            <span data-currency-iso="NGN">₦</span>
+                            <span><?=$product->old_price?></span>
+                        </span>
                     </div>
-                </a>
-            <?php endforeach; ?>
-        </div>
-    </div>
-    <div class="tab-pane fade" id="manage-view" role="tabpanel" aria-labelledby="manage-view-tab">
-        <ul class="list-group manage-tile-tab">
-            <?php foreach ($products as $product): ?>
-                <a href="/categories/<?=$product->category_id?>/products/<?=$product->id?>" class="product-tile list-group-item">
-                    <li class="d-flex justify-content-between align-items-center">
-                        <?=$product->name?>
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </li>
-                </a>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-
-<!--     <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div> -->
+                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+            </div>
+        </a>
+    <?php endforeach; ?>
 </div>
+
 
 <div class=""><?=$pagination_view?></div>
 
